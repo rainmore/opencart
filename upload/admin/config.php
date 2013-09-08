a9a541b65610d055b8a305942c86f6a0e360fb40
Jie Rong (Felix) <rainmore24@gmail.com>
2013-09-08 21:14:19 +1000

    install

---
+++ upload/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.dev/admin/');
+define('HTTP_CATALOG', 'http://opencart.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.dev/admin/');
+define('HTTPS_CATALOG', 'http://opencart.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/jrong/Sites/opencart/upload/admin/');
+define('DIR_SYSTEM', '/Users/jrong/Sites/opencart/upload/system/');
+define('DIR_DATABASE', '/Users/jrong/Sites/opencart/upload/system/database/');
+define('DIR_LANGUAGE', '/Users/jrong/Sites/opencart/upload/admin/language/');
+define('DIR_TEMPLATE', '/Users/jrong/Sites/opencart/upload/admin/view/template/');
+define('DIR_CONFIG', '/Users/jrong/Sites/opencart/upload/system/config/');
+define('DIR_IMAGE', '/Users/jrong/Sites/opencart/upload/image/');
+define('DIR_CACHE', '/Users/jrong/Sites/opencart/upload/system/cache/');
+define('DIR_DOWNLOAD', '/Users/jrong/Sites/opencart/upload/download/');
+define('DIR_LOGS', '/Users/jrong/Sites/opencart/upload/system/logs/');
+define('DIR_CATALOG', '/Users/jrong/Sites/opencart/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'c2oi-root');
+define('DB_PASSWORD', 'rainmore24');
+define('DB_DATABASE', 'c2oi');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ upload/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/jrong/Sites/opencart/upload/catalog/');
+define('DIR_SYSTEM', '/Users/jrong/Sites/opencart/upload/system/');
+define('DIR_DATABASE', '/Users/jrong/Sites/opencart/upload/system/database/');
+define('DIR_LANGUAGE', '/Users/jrong/Sites/opencart/upload/catalog/language/');
+define('DIR_TEMPLATE', '/Users/jrong/Sites/opencart/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/jrong/Sites/opencart/upload/system/config/');
+define('DIR_IMAGE', '/Users/jrong/Sites/opencart/upload/image/');
+define('DIR_CACHE', '/Users/jrong/Sites/opencart/upload/system/cache/');
+define('DIR_DOWNLOAD', '/Users/jrong/Sites/opencart/upload/download/');
+define('DIR_LOGS', '/Users/jrong/Sites/opencart/upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'c2oi-root');
+define('DB_PASSWORD', 'rainmore24');
+define('DB_DATABASE', 'c2oi');
+define('DB_PREFIX', 'oc_');
+?>
